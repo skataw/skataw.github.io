c579ac61a27cdd6c050b402a7cef1311134e6f19
Steven <skata@tricitiesrop.org>
2017-11-13 14:09:23 -0800

    Added initial SQL info/resources

---
+++ SQL.sql
@@ -0,0 +1,12 @@
+https://www.sqlteaching.com
+
+FROM <TABLENAME>
+
+- When selecting from a single table - FROM is pretty easy
+	- FROM <TABLENAME>
+	- More on multiple tables later in this course
+- You *should* qualify all the columns in the select list with the Table's name
+
+
+SELECT DISTINCT p.person first name FROM person p;
+
